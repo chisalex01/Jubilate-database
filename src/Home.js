103b559c34c216fe68ae41ce0f7da918b821efc9
chisalex01 <chisalex01@yahoo.com>
2022-12-16 14:02:14 +0200

    Add blog elements

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,47 @@
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new website",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 1,
+    },
+    {
+      title: "Welcome party",
+      body: "lorem ipsum...",
+      author: "yoshi",
+      id: 2,
+    },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+
+  const [name, setName] = useState("Mario");
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    console.log("use effect ran");
+    console.log(name);
+  }, [name]);
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      <BlogList
+        blogs={blogs}
+        title={"All blogs!"}
+        handleDelete={handleDelete}
+      />
     </div>
   );
 };
